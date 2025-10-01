92998f739cc61f50bd83c9d09ce16553c267da42
Mário Melo <mariofdemelo@gmail.com>
2025-10-01 13:46:16 +0200

    Defaults to english

--- public/app.js
+++ public/app.js
@@ -112,7 +112,7 @@ function updateUI() {
 
     // Update guessed letters
     if (currentGameState.guesses.length === 0) {
-        guessedLettersDisplay.textContent = t('none');
+        guessedLettersDisplay.textContent = 'None';
     } else {
         guessedLettersDisplay.textContent = currentGameState.guesses.join(', ');
     }
